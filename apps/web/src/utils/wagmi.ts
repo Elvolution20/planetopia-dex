368d45ce0697af486508a12ac3a0b19dd2f2270d
Elvolution <75731970+Elvolution20@users.noreply.github.com>
2023-05-25 22:38:14 +0100

    commit

--- apps/web/src/utils/wagmi.ts
+++ apps/web/src/utils/wagmi.ts
@@ -100,7 +100,7 @@ export const injectedConnector = new InjectedConnector({
 export const coinbaseConnector = new CoinbaseWalletConnector({
   chains,
   options: {
-    appName: 'PancakeSwap',
+    appName: 'PlanetopiaSwap',
     appLogoUrl: 'https://pancakeswap.com/logo.png',
   },
 })
