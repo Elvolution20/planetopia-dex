5a978efe29036c7b0abf7692d2aff52f0ee29880
Elvolution <75731970+Elvolution20@users.noreply.github.com>
2023-05-25 22:43:35 +0100

    commit

--- packages/uikit/src/widgets/Swap/Footer.tsx
+++ packages/uikit/src/widgets/Swap/Footer.tsx
@@ -1,7 +1,7 @@
 import { memo, ReactNode } from "react";
 import styled from "styled-components";
 import { useTranslation } from "@pancakeswap/localization";
-import { LinkExternal, Flex, Svg, Image, Button } from "../../components";
+import { LinkExternal, Flex } from "../../components";
 
 const Wrapper = styled.div<{ $isSide: boolean }>`
   width: 100%;
@@ -18,23 +18,6 @@ const Wrapper = styled.div<{ $isSide: boolean }>`
   }
 `;
 
-const BubbleWrapper = styled(Flex)`
-  svg {
-    fill: ${({ theme }) => theme.colors.textSubtle};
-    transition: background-color 0.2s, opacity 0.2s;
-  }
-  &:hover {
-    svg {
-      opacity: 0.65;
-    }
-  }
-  &:active {
-    svg {
-      opacity: 0.85;
-    }
-  }
-`;
-
 type FooterVariant = "default" | "side";
 
 const Footer: React.FC<
@@ -47,10 +30,8 @@ const Footer: React.FC<
   }>
 > = ({
   variant = "default",
-  helpUrl,
   externalText,
   externalLinkUrl,
-  helpImage = <Image src="https://cdn.pancakeswap.com/help/help.png" alt="Get some help" width={160} height={108} />,
 }) => {
   const { t } = useTranslation();
   const isSide = variant === "side";
@@ -67,25 +48,6 @@ const Footer: React.FC<
           >
             {externalText}
           </LinkExternal>
-        </Flex>
-      )}
-      {isSide && <Flex flexGrow={1} />}
-      {helpUrl && (
-        <Flex
-          flexGrow={isSide ? 0 : 1}
-          alignItems="center"
-          width={["100%", "100%", "100%", isSide ? "100%" : "auto"]}
-          justifyContent={["center", "center", "center", "flex-end"]}
-        >
-          <BubbleWrapper>
-            <Button id="clickExchangeHelp" as="a" external href={helpUrl} variant="subtle">
-              {t("Need help ?")}
-            </Button>
-            <Svg viewBox="0 0 16 16">
-              <path d="M0 16V0C0 0 3 1 6 1C9 1 16 -2 16 3.5C16 10.5 7.5 16 0 16Z" />
-            </Svg>
-          </BubbleWrapper>
-          {helpImage}
         </Flex>
       )}
     </Wrapper>
