cbe2b4b61328e47e8fb02ec1c97ae0d39583bdea
Elvolution <75731970+Elvolution20@users.noreply.github.com>
2024-01-07 08:18:32 +0100

    Revert "Update on Fon2"
    This reverts commit b32745acab03218f89647f6b7d5ec070e2c55925.

--- packages/tokens/src/201022.ts
+++ packages/tokens/src/201022.ts
@@ -1,8 +1,8 @@
 import { ChainId, ERC20Token, WFON } from '@pancakeswap/sdk'
-import {  PLANET_FSC, USDT_FSC } from './common'
+import { PLANET, USDT_FSC } from './common'
 
 export const fscTokens = {
   wfon: WFON[ChainId.FSC],
   usdt: USDT_FSC,
-  planet: PLANET_FSC,
+  planet: PLANET,
 }
